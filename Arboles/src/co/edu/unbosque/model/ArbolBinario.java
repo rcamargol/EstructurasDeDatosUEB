7a9655c67f0028900462a045797da6fca86b3156
rcamargol <rcamargol@unbosque.edu.co>
2022-04-25 22:41:16 -0500

    cambios en estetica

--- Arboles/src/co/edu/unbosque/model/ArbolBinario.java
+++ Arboles/src/co/edu/unbosque/model/ArbolBinario.java
@@ -7,27 +7,21 @@ public class ArbolBinario {
 	public ArbolBinario() {
 		this.raiz = null;
 	}
-	
 	public ArbolBinario(NodoA raiz) {
 		this.raiz = raiz;
 	}
-	
 	public boolean arbolVacio() {
 		return this.raiz == null;
 	}
-	
 	public NodoA nuevoArbol(NodoA izquierda, int info, NodoA derecha) {
 		return new NodoA(izquierda, info, derecha);
 	}
-
 	public NodoA getRaiz() {
 		return raiz;
 	}
-
 	public void setRaiz(NodoA raiz) {
 		this.raiz = raiz;
 	}
-	
 	
 }
 
